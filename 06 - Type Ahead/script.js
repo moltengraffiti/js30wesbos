17710f74d9435f13eb622839fbb95dca82d05122
Sam Reilly <samreilly@gmail.com>
2021-07-05 18:41:42 +1000

    starting displayMatches fn

--- 06 - Type Ahead/script.js
+++ 06 - Type Ahead/script.js
@@ -16,17 +16,38 @@ const cities = [];
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
+    //if we use cities.push(data) we'll end up with an array of length 1, an array of an array
+    //se we use ES6 spread operator
+    //TODO - look at spread
     
-console.log(cities);
+//console.log(cities);
     
 //find what's typed and filter the list
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
-        //look for a match
-
+        //look for a match in cities or states
         //need to create a regex to pass a variable in
-        const regex = new RegExp(wordToMatch, '')
-        return place.city.match()
-    })
+        const regex = new RegExp(wordToMatch, 'gi')
+        return place.city.match(regex) || place.state.match(regex);
+    });
     
 }
+
+function displayMatches() {
+    console.log(this.value);
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('change', displayMatches);
+
+
+//Not suitable due to THIS
+// display2 =() => {
+//     console.log(`In the display 2 arrow function`)
+//     console.log(this.value);
+// }
+
+
